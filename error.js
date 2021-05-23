69b0fade95cbb40a23ef5694f208c16e41f0a6d8
Adurinti Ashok <79994620+AdurintiAshok@users.noreply.github.com>
2021-05-23 11:50:12 +0530

    Add files via upload

---
+++ error.js
@@ -0,0 +1,12 @@
+function login(){
+    var x=document.getElementById("username").value;
+    var y=document.getElementById("possoword").value;
+    if(x=="ashok" && y=="1234")
+    {
+        alert(9);
+    }
+    else{
+        alert(5);
+    }
+
+}
